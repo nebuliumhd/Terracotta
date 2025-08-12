a1bc5db268531dc9ba5ea19c9f0a9984768df60a
nebuliumhd <garlandj530@gmail.com>
2025-08-12 16:32:01 -0400

    Add project files.

---
+++ Terracotta/Terracotta.cpp
@@ -0,0 +1,12 @@
+﻿// Terracotta.cpp : Defines the entry point for the application.
+//
+
+#include "Terracotta.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
